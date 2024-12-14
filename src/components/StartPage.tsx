29d9600c686dcc27e99362058886a76b4b695199
ABHAY <abhaybalakrishnan884@gmail.com>
2024-12-14 18:54:31 +0530

    start page ui completed

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -4,23 +4,35 @@ import Image from "next/image";
 import TITLE from "@/assets/images/title.png";
 
 import Navbar from "./SpecialNavbar";
+import ShipSelector from "./ShipSelector";
 
 const StartPage = ({ onStart }: { onStart: () => void }) => {
   return (
-    <div className="flex relative bg-darkIndigo_bg max-w-sm mx-auto max-h-screen pb-[10px] overflow-x-hidden flex-col gap-[27px] items-center h-screen font-pixeboy justify-between">
+    <div className="flex relative bg-darkIndigo_bg max-w-sm mx-auto max-h-screen pb-[10px] overflow-x-hidden flex-col gap-[21px] items-center h-screen font-pixeboy justify-between">
       <Navbar />
 
       <Image
         src={TITLE}
         alt="DOODLE BLAST"
-        className="max-w-[264px] mt-[170px]"
+        className="max-w-[256px] mt-[160px]"
       />
+      
+      <div className="flex flex-col gap-[8px] mt-[-2px]">
+        <div className="flex flex-col justify-center items-center gap-0">
+          <p className="text-[30px] text-white ">select ship</p>
+          <p className="text-skyblue_btn text-[18px] mt-[-7px]">SHIP 101</p>
+        </div>
+        
+        <div className="">
+          <ShipSelector />
+        </div>
+      </div>
 
-      <div className="flex flex-col gap-[27px]">
+      <div className="flex flex-col gap-[27px] mt-[-4px]">
         <div className="flex justify-center gap-[6px] items-center flex-col text-[18px]">
           <div className="text-center flex flex-col text-cherryPink_text">
-            <p>YOUR RANK : </p>
-            <p className="mt-[-6px]">YOUR HIGH SCORE : </p>
+            <p>YOUR RANK : 20XX</p>
+            <p className="mt-[-8px]">YOUR HIGH SCORE : 4XX</p>
           </div>
 
           <div className="text-center flex flex-col text-[16px]">
