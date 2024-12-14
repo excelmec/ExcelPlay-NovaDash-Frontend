793bc72d3067e0063f8c616b3fac77f5f2fb0a2d
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2024-12-14 19:37:58 +0530

    Pushed New Changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
