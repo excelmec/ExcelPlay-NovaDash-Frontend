76b93e9ae576d04cfd7b7b6dfb2c63e30b19b274
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2024-12-14 20:11:12 +0530

    Image Optimization Turned Off

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // Disables built-in image optimization
+  },
 };
 
 export default nextConfig;
