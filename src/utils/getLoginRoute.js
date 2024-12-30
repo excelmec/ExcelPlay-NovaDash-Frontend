b92d564bc10c0e77b370e310beec8b2521171f69
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2024-12-30 14:59:06 +0530

    Auth Test

--- src/utils/getLoginRoute.js
+++
@@ -1,9 +0,0 @@
-export function getLoginUrl() {
-    const authRedirBaseUrl = process.env.REACT_APP_AUTH_REDIR_URL;
-
-    const currentUrl = new URL(window.location.href);
-    const authUrl = new URL(authRedirBaseUrl);
-    authUrl.searchParams.append('redirect_to', currentUrl.toString());
-
-    return authUrl.toString();
-}
